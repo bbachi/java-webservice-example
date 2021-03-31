3309e5b557bf03384420feaef063a04336c7e393
bbachi <bhargavbachina@yahoo.com>
2021-03-31 08:33:06 -0500

    Docker chnages

--- Dockerfile
+++
@@ -1,4 +0,0 @@
-FROM dockerimages-prd.jbhunt.com/jbh-alpine-jre11:latest
-EXPOSE 8080
-ADD target/todo.jar todo.jar
-ENTRYPOINT exec java $JAVA_OPTS -XX:TieredStopAtLevel=1 -client -jar todo.jar
---
+++ target/generated-sources/java/com/example/todo/entity/QTask.java
@@ -0,0 +1,41 @@
+package com.example.todo.entity;
+
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.Generated;
+import com.querydsl.core.types.Path;
+
+
+/**
+ * QTask is a Querydsl query type for Task
+ */
+@Generated("com.querydsl.codegen.EntitySerializer")
+public class QTask extends EntityPathBase<Task> {
+
+    private static final long serialVersionUID = -1294153991L;
+
+    public static final QTask task = new QTask("task");
+
+    public final StringPath description = createString("description");
+
+    public final NumberPath<Long> id = createNumber("id", Long.class);
+
+    public final StringPath name = createString("name");
+
+    public QTask(String variable) {
+        super(Task.class, forVariable(variable));
+    }
+
+    public QTask(Path<? extends Task> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QTask(PathMetadata metadata) {
+        super(Task.class, metadata);
+    }
+
+}
+
